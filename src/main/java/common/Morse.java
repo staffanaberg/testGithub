7bb82af96980348e7e1a9ef189f599b99bce8e58
Staffan Åberg <staffan.aaberg@gmail.com>
2024-03-07 10:04:16 +0100

    La in ett utropstecken

--- src/main/java/common/Morse.java
+++ src/main/java/common/Morse.java
@@ -3,7 +3,13 @@ package common;
 public class Morse {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 
 }
+
+
+//Commit CTRL-K
+//Välj de filer som ska skickas till Github (om det inte är automatiskt redan korrekt)
+//Skriv en kommentar
+//Commit and Push...
